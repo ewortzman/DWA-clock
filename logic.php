644834fa9f6d7b18032b3d6ecafb0238a8e4970f
Ethan Wortzman <ethanwortzman@g.harvard.edu>
2014-07-03 15:10:01 -0400

    updated logic.php

--- logic.php
+++ logic.php
@@ -6,14 +6,10 @@ $hour = date("H");
 
 if ($hour>=5 && $hour<11){
    $timeOfDay = "morning";
-   $background = "#865f86";
 } else if($hour>=11 && $hour<16){
    $timeOfDay = "afternoon";
-   $background = "#2c87c8";
 } else if($hour>=16 && $hour<20){
    $timeOfDay = "evening";
-   $background = "#c7b02f";
 } else {
    $timeOfDay = "night";
-   $background = "#180629";
 }
