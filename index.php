5b9ebd136745fe263ccf46142fa1fe43e4628d52
Ethan Wortzman <ethanwortzman@g.harvard.edu>
2014-07-03 15:07:45 -0400

    used better technique for dynamic CSS

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
    <head>
       <?php require "logic.php"; ?>
 
-      <link rel='stylesheet' type='text/css' href='stylesheets/default.php' />
+      <link rel='stylesheet' type='text/css' href='stylesheets/default.css' />
 
    </head>
 
-<body>
+<body id="<?php echo $timeOfDay ?>">
    <h1>It's <?php echo date("h:ia"); ?></h1>
    <h5><?php echo date("e"); ?></h5>
 
